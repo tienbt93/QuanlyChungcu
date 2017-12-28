fe076d59eae5983ec2ffb21793bf1308da4232c8
Tieu Tu <trantien.st.1993@gmail.com>
2017-12-28 23:36:29 +0700

    add database and DAO

---
+++ src/DAO/ChitietGiaodichDAO.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DAO;
+
+import Interface.InterfaceDAO;
+import java.util.List;
+
+/**
+ *
+ * @author PTIT
+ */
+public class ChitietGiaodichDAO extends InterfaceDAO{
+
+    @Override
+    public Object add(Object ob) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Object getId(int id) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Object delete(int id) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Object update(Object ob) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<Object> getAll() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
---
+++ src/DAO/ToanhaDAO.java
@@ -0,0 +1,178 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DAO;
+
+import Interface.InterfaceDAO;
+import Model.Chungcu;
+import Model.Toanha;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author PTIT
+ */
+public class ToanhaDAO extends InterfaceDAO {
+
+    public ToanhaDAO() {
+    }
+
+    @Override
+    public Object add(Object ob) {
+        if (ob instanceof Toanha) {
+            Toanha toanha = (Toanha) ob;
+            String sql = "INSERT INTO [dbo].[toanha]\n"
+                    + "           ([ten_toanha]\n"
+                    + "           ,[diachi]\n"
+                    + "           ,[sdt_quanly]\n"
+                    + "           ,[so_tang]\n"
+                    + "           ,[so_canho]\n"
+                    + "           ,[tong_dientich]\n"
+                    + "           ,[ngay_xaydung]\n"
+                    + "           ,[ngay_hoanthanh]\n"
+                    + "           ,[mota]\n"
+                    + "           ,[id_chungcu])\n"
+                    + "     VALUES(?,?,?,?,?,?,?,?,?,?)";
+            try {
+                PreparedStatement ps = myConnect.getConn().prepareStatement(sql);
+                ps.setNString(1, toanha.getTenToanha());
+                ps.setNString(2, toanha.getDiachi());
+                ps.setNString(3, toanha.getSdtQuanly());
+                ps.setInt(4, toanha.getSoTang());
+                ps.setInt(5, toanha.getSoCanho());
+                ps.setDouble(6, toanha.getTongDientich());
+                ps.setDate(7, toanha.getNgayXaydung());
+                ps.setDate(8, toanha.getNgayHoanthanh());
+                ps.setString(9, toanha.getMota());
+                ps.setInt(10, toanha.getChungcu().getIdChungcu());
+                return ps.executeUpdate() > 0;
+            } catch (SQLException ex) {
+                Logger.getLogger(ToanhaDAO.class.getName()).log(Level.SEVERE, null, ex);
+            }
+
+            return false;
+        } else {
+            return false;
+        }
+    }
+
+    @Override
+    public Object delete(int id) {
+        String sql = "delete from [dbo].[toanha] where id_toanha=?";
+        try {
+            PreparedStatement ps = myConnect.getConn().prepareStatement(sql);
+            ps.setInt(1, id);
+            return ps.executeUpdate() > 0;
+        } catch (SQLException ex) {
+            Logger.getLogger(ToanhaDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return false;
+    }
+
+    @Override
+    public Object update(Object ob) {
+        if (ob instanceof Toanha) {
+            Toanha toanha = (Toanha) ob;
+            String sql = "UPDATE [dbo].[toanha]\n"
+                    + "   SET [ten_toanha] = ?\n"
+                    + "      ,[diachi] = ?\n"
+                    + "      ,[sdt_quanly] = ?\n"
+                    + "      ,[so_tang] = ?\n"
+                    + "      ,[so_canho] = ?\n"
+                    + "      ,[tong_dientich] = ?\n"
+                    + "      ,[ngay_xaydung] = ?\n"
+                    + "      ,[ngay_hoanthanh] = ?\n"
+                    + "      ,[mota] = ?\n"
+                    + "      ,[id_chungcu] = ?"
+                    + " WHERE id_toanha=? ";
+            try {
+                PreparedStatement ps = myConnect.getConn().prepareStatement(sql);
+                ps.setNString(1, toanha.getTenToanha());
+                ps.setNString(2, toanha.getDiachi());
+                ps.setNString(3, toanha.getSdtQuanly());
+                ps.setInt(4, toanha.getSoTang());
+                ps.setInt(5, toanha.getSoCanho());
+                ps.setDouble(6, toanha.getTongDientich());
+                ps.setDate(7, toanha.getNgayXaydung());
+                ps.setDate(8, toanha.getNgayHoanthanh());
+                ps.setString(9, toanha.getMota());
+                ps.setInt(10, toanha.getChungcu().getIdChungcu());
+                ps.setInt(11, toanha.getIdToanha());
+                return 0 <= ps.executeUpdate();
+            } catch (SQLException ex) {
+                Logger.getLogger(ToanhaDAO.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public Object getAll() {
+        List<Toanha> arr = null;
+        String sql = "select * from Toanha";
+        ResultSet rs = myConnect.executeQuery(sql);
+        ChungcuDAO chungcuDAO = new ChungcuDAO();
+        if (rs != null) {
+            arr = new ArrayList<>();
+            try {
+                Toanha toanha = null;
+                while (rs.next()) {
+                    toanha = new Toanha();
+                    toanha.setIdToanha(rs.getInt(1));
+                    toanha.setTenToanha(rs.getNString(2));
+                    toanha.setDiachi(rs.getNString(3));
+                    toanha.setSdtQuanly(rs.getNString(4));
+                    toanha.setSoTang(rs.getInt(5));
+                    toanha.setSoCanho(rs.getInt(6));
+                    toanha.setTongDientich(rs.getInt(7));
+                    toanha.setNgayXaydung(rs.getDate(8));
+                    toanha.setNgayHoanthanh(rs.getDate(9));
+                    toanha.setMota(rs.getNString(10));
+                    toanha.setChungcu((Chungcu) chungcuDAO.getId(rs.getInt(11)));
+                    arr.add(toanha);
+                }
+            } catch (SQLException ex) {
+                Logger.getLogger(ToanhaDAO.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+        return arr;
+    }
+
+    @Override
+    public Object getId(int id) {
+        Toanha toanha = null;
+        String sql = "select * from toanha where id_toanha=" + id;
+        ResultSet rs = myConnect.executeQuery(sql);
+        ChungcuDAO chungcuDAO = new ChungcuDAO();
+        try {
+            if (rs.next()) {
+                toanha = new Toanha();
+                toanha.setIdToanha(rs.getInt(1));
+                toanha.setTenToanha(rs.getNString(2));
+                toanha.setDiachi(rs.getNString(3));
+                toanha.setSdtQuanly(rs.getNString(4));
+                toanha.setSoTang(rs.getInt(5));
+                toanha.setSoCanho(rs.getInt(6));
+                toanha.setTongDientich(rs.getInt(7));
+                toanha.setNgayXaydung(rs.getDate(8));
+                toanha.setNgayHoanthanh(rs.getDate(9));
+                toanha.setMota(rs.getNString(10));
+                toanha.setChungcu((Chungcu) chungcuDAO.getId(rs.getInt(11)));
+
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(ToanhaDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        return toanha;
+    }
+
+}
